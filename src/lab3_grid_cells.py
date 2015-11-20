3cb498447b71330976d55b9156da4806fe4c6e11
WPI_RBE <nathaniel.jefferson@gmail.com>
2015-11-20 02:08:22 -0500

    change to lab3_grid_cells.py to add mulitple gridpaths

--- src/lab3_grid_cells.py
+++ src/lab3_grid_cells.py
@@ -26,8 +26,10 @@ def mapCallBack(data):
     #print data.info # used for debugging
     #publishCells(mapgrid.data) # used for debugging
 
-def publishCells(grid):
+def publishCells(grid,num):
     global pub
+    global ckd
+    global front
     print "publishing"
     k=0
     cells = GridCells()
@@ -38,7 +40,7 @@ def publishCells(grid):
     for i in range(1,height): #height should be set to hieght of grid
         for j in range(1,width): #height should be set to hieght of grid
             #print k # used for debugging
-            if (grid[k] == 100):
+            if (grid[k] == num):
                 point=Point()
                 point.x=j*.3+.32 # edit for grid size
                 point.y=i*.3-.15 # edit for grid size
@@ -46,7 +48,7 @@ def publishCells(grid):
                 cells.cells.append(point)
             k=k+1
         k=k+1
-        if (grid[k] == 100):
+        if (grid[k] == num):
             point=Point()
             point.x=j*.3+.62 # edit for grid size
             point.y=i*.3-.15 # edit for grid size
@@ -54,14 +56,84 @@ def publishCells(grid):
             cells.cells.append(point)
 
     #print cells # used for debugging
-    pub.publish(cells)           
+    if(num == 100):
+        pub.publish(cells)           
+    if(num == 1):
+        ckd.publish(cells)
+    if(num == 2):
+        front.publish(cells)
+
+def publishFront(grid):
+    global front
+    print "publishing"
+    k=0
+    cells = GridCells()
+    cells.header.frame_id = 'map'
+    cells.cell_width = 0.3 # edit for grid size .3 for simple map
+    cells.cell_height = 0.3 # edit for grid size
+
+    for i in range(1,10): #height should be set to hieght of grid
+        for j in range(1,9): #height should be set to hieght of grid
+            #print k # used for debugging
+            if (grid[k] == 1):
+                point=Point()
+                point.x=j*.3+.32 # edit for grid size
+                point.y=i*.3-.15 # edit for grid size
+                point.z=0
+                cells.cells.append(point)
+            k=k+1
+        k=k+1
+        if (grid[k] == 1):
+            point=Point()
+            point.x=j*.3+.62 # edit for grid size
+            point.y=i*.3-.15 # edit for grid size
+            point.z=0
+            cells.cells.append(point)
+
+    #print cells # used for debugging
+    front.publish(cells)           
+
+def publishChecked(grid):
+    global ckd
+    print "publishing"
+    k=0
+    cells = GridCells()
+    cells.header.frame_id = 'map'
+    cells.cell_width = 0.3 # edit for grid size .3 for simple map
+    cells.cell_height = 0.3 # edit for grid size
+
+    for i in range(1,10): #height should be set to hieght of grid
+        for j in range(1,9): #height should be set to hieght of grid
+            #print k # used for debugging
+            if (grid[k] == 0):
+                point=Point()
+                point.x=j*.3+.32 # edit for grid size
+                point.y=i*.3-.15 # edit for grid size
+                point.z=0
+                cells.cells.append(point)
+            k=k+1
+        k=k+1
+        if (grid[k] == 0):
+            point=Point()
+            point.x=j*.3+.62 # edit for grid size
+            point.y=i*.3-.15 # edit for grid size
+            point.z=0
+            cells.cells.append(point)
+
+    #print cells # used for debugging
+    ckd.publish(cells)           
+
 
 #Main handler of the project
 def run():
     global pub
+    global ckd
+    global front
     rospy.init_node('lab3')
     sub = rospy.Subscriber("/map", OccupancyGrid, mapCallBack)
     pub = rospy.Publisher("/grid_walls", GridCells, queue_size=1)
+    ckd = rospy.Publisher("/grid_checked", GridCells, queue_size=1)
+    front = rospy.Publisher("/grid_Front", GridCells, queue_size=1)
     sleeper = rospy.Duration(1)
     rospy.sleep(sleeper)
 
@@ -81,7 +153,16 @@ def run():
 1.0,0.0,1.0,0.0,1.0,0.0,1.0,0.0,1.0,0.0]
 
     while (1 and not rospy.is_shutdown()):
-        publishCells(mapData) #publishing map data every 2 seconds
+        publishCells(mapData,100) #publishing map data every 2 seconds
+        publishCells(mapData,2)
+        publishCells(mapData,1)
+        for i in range(1, 100):
+            if(mapData[i] == 1):
+                print mapData[i]
+        #        mapData[i] = 0
+            else:
+                print mapData[i]
+        #        mapData[i] = 1
         rospy.sleep(2)  
         print("Complete")
     
